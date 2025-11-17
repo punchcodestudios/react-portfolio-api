e58f0d07c8d5b86e10b4e6bac3440d42a745f895
Patrick Schandler <pschandler@gmail.com>
2025-11-17 16:12:26 -0600

    update cors config in index.js

--- index.js
+++ index.js
@@ -77,9 +77,14 @@ console.log("before cors check isDev: ", isDev);
 // if (isDev) {
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
-    methods: ["POST", "PUT", "GET", "DELETE"],
-    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173", // Vite dev server
+      "https://punchcodestudios.com",
+    ],
+    credentials: false, // Set to true only if you need cookies
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
 // }
